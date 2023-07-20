ddd0271298113eb2b6fc6f0aae816f32a2643ec6
otherpeopIe <mackenzierirving@gmail.com>
2023-07-19 22:55:55 -0400

    Lesson 4: Classes/Types

--- main.py
+++ main.py
@@ -4,18 +4,24 @@
 #Start function, launches our game
 def Start(): 
     print("Hello World!") #intro message
-    CreatePlayer()
+    newPlayer = CreatePlayer()
+    print("HP: " + newPlayer.health)
     return "bye" #returns exit message
 
 #Create Player function handles player creation
+
 def CreatePlayer():
     name = input("Enter your name: ") #built in input takes string and returns input string
     print("Based. Welcome " + name) #print welcome message with player name
-    SelectClass() 
+    faction = SelectFaction()
+    print("Indeed. Welcome " + name + ", a wise " + faction)
+    health = 100
+    createdPlayer = Player(name, faction, health)
+    return createdPlayer
 
    
 #Handles validating players class selection
-def SelectClass():
+def SelectFaction():
     exit = False #bool to use to exit loop -- (Bool is True or False)
     while(exit == False): #if bool 'exit' is true, exit loop
                           #while exit is false, run loop code
@@ -23,11 +29,12 @@ def SelectClass():
         print("Select class")
         print("a: Knight")
         print("b: Mage")
-        print("c. Druid")
-        print("d. Shaman")
+        print("c: Druid")
+        print("d: Shaman")
         
         classChoice = input() #input without prompt (no string input) 
                               #string is any text
+                              #reading input suspends the thread
         
         if (classChoice == "a"): # == is saying is it equal to (like a question, does this equal this?), = is an assignment (this now equals this)
             classChoice = "Knight"
@@ -43,9 +50,26 @@ def SelectClass():
 
         #end of while loop, jump back to top and check if exit is true
 
-    print("Exited while loop")
+    return classChoice 
 
-   
+#declaring a new class -- this is how you create a new type
+#definition
+class Player:
+    def __init__(self, name, faction, health):
+        self.name = name
+        self.faction = faction
+        self.health = health
+
+# definition vs. instance
+# definition -- we are declaring a new class, creating a new type using the class keyword
+# a class has a constructor/initializer. this is a function that gets called when creating an instance of this type 
+#
+# instance -- we could have one or many instances of our type 
+# think apple -- we have the definition, which is found in a dictionary,
+# and we have our instance, which is in our kitchen
+
+#Reminder: arrays next lesson
+
 
 
 #entry point
